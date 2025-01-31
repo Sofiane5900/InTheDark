6cf0d64a20a4ab52018d0a67ff11d370c365385a
bafodej <72113145+bafodej@users.noreply.github.com>
2025-01-31 09:25:09 +0100

    passage tilemap  => tilemaplayer

--- DonjonEtHeros/BattleManager.cs
+++ DonjonEtHeros/BattleManager.cs
@@ -4,48 +4,48 @@ using Godot;
 
 public partial class BattleManager : Node
 {
-    public static BattleManager Instance { get; private set; }
-    private Node gameNode;
-    public BaseEnemy CurrentEnemy { get; private set; }
+	public static BattleManager Instance { get; private set; }
+	private Node gameNode;
+	public BaseEnemy CurrentEnemy { get; private set; }
 
-    public override void _Ready()
-    {
-        if (Instance is null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            QueueFree();
-            return;
-        }
-        gameNode = GetTree().Root.GetNode("Valombre") as Node;
-    }
+	public override void _Ready()
+	{
+		if (Instance is null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			QueueFree();
+			return;
+		}
+		gameNode = GetTree().Root.GetNode("Valombre") as Node;
+	}
 
-    public static async Task StartBattle(string battleScene, string enemyName)
-    {
-        PackedScene battleScenePath = GD.Load<PackedScene>($"res://Scenes/{battleScene}.tscn");
-        if (battleScenePath is null)
-        {
-            GD.PrintErr($"Battle scene {battleScene} not found");
-        }
-        BaseEnemy enemyPath = GD.Load<BaseEnemy>($"res://Ressources/{enemyName}.tres");
-        if (enemyPath is null)
-        {
-            GD.PrintErr($"Enemy {enemyName} not found");
-        }
+	public static async Task StartBattle(string battleScene, string enemyName)
+	{
+		PackedScene battleScenePath = GD.Load<PackedScene>($"res://Scenes/{battleScene}.tscn");
+		if (battleScenePath is null)
+		{
+			GD.PrintErr($"Battle scene {battleScene} not found");
+		}
+		BaseEnemy enemyPath = GD.Load<BaseEnemy>($"res://Ressources/{enemyName}.tres");
+		if (enemyPath is null)
+		{
+			GD.PrintErr($"Enemy {enemyName} not found");
+		}
 
-        await Instance?.StartBattleInstance(battleScenePath, enemyPath);
-    }
+		await Instance?.StartBattleInstance(battleScenePath, enemyPath);
+	}
 
-    private async Task StartBattleInstance(PackedScene battleScene, BaseEnemy enemy)
-    {
-        // focus on this later
-        // PauseOverworld();
+	private async Task StartBattleInstance(PackedScene battleScene, BaseEnemy enemy)
+	{
+		// focus on this later
+		// PauseOverworld();
 
-        Battle battleMap = battleScene.Instantiate<Battle>();
-        gameNode.AddChild(battleMap);
-        battleMap.Name = "BattleMap";
-        GD.Print($"battleMap: {battleMap.Name}, battleScene: {battleScene.ResourceName}");
-    }
+		Battle battleMap = battleScene.Instantiate<Battle>();
+		gameNode.AddChild(battleMap);
+		battleMap.Name = "BattleMap";
+		GD.Print($"battleMap: {battleMap.Name}, battleScene: {battleScene.ResourceName}");
+	}
 }
--- DonjonEtHeros/State.cs
+++ DonjonEtHeros/State.cs
@@ -3,13 +3,13 @@ using Godot;
 
 public partial class State : Node
 {
-    // public static bool CanMove;
-    public string AppleStatus = "";
+	// public static bool CanMove;
+	public string AppleStatus = "";
 
-    public int CurrentHealth = 10;
-    public int MaxHealth = 100;
-    public int Damage = 10;
+	public int CurrentHealth = 10;
+	public int MaxHealth = 100;
+	public int Damage = 10;
 
-    // has_met variables
-    public bool has_met_pouleto = false;
+	// has_met variables
+	public bool has_met_pouleto = false;
 }
