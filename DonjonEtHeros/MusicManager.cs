fa9b7fe4e6df4bb96a5a842fdd0cf1738529ea29
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-02-03 16:54:20 +0100

    ajout de méthode play music

--- DonjonEtHeros/MusicManager.cs
+++ DonjonEtHeros/MusicManager.cs
@@ -6,8 +6,30 @@ public partial class MusicManager : Node
     private static MusicManager Instance;
     private AudioStreamPlayer musicPlayer;
 
-    private override void _Ready()
+    public override void _Ready()
     {
         if (Instance is null)
+        {
+            QueueFree();
+        }
+        else
+        {
+            Instance = this;
+        }
+        musicPlayer = new AudioStreamPlayer();
+        AddChild(musicPlayer);
+        musicPlayer.Autoplay = true;
+    }
+
+    public void PlayMusic(AudioStream music)
+    {
+        // Si la musique est déjà en cours de lecture, on ne la relance pas
+        if (musicPlayer.Stream == music)
+        {
+            return;
+        }
+        // Le stream doit jouer notre musique
+        musicPlayer.Stream = music;
+        musicPlayer.Play();
     }
 }
